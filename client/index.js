6a1278c6aa2eefd1226d2a1763cbe6a75ce923aa
jaimebaik <jaimebaik5@gmail.com>
2020-10-27 14:45:10 -0700

    added webpack till scss

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import styles from './scss/styles.css';
+
+render(
+  <p>this is from index js</p>,
+  document.getElementById('app'),
+);
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//parising request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
